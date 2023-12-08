f0a6b0df11a0ca89fed3ededfb19f8a092ec40fc
Júlio Nepomuceno <julioreplay@gmail.com>
2023-12-07 22:42:41 -0300

    [feature]: improving layout

--- src/pages/lading-page/index.tsx
+++ src/pages/lading-page/index.tsx
@@ -2,11 +2,11 @@ import { Header } from "../../components";
 
 function LandingPage() {
   return (
-    <div className="bg-main-white dark:bg-dark-main-black h-screen px-10">
+    <div className="bg-main-white dark:bg-dark-main-black h-screen px-10 xl:px-48 ">
       <Header />
       <main className="mt-20 flex flex-col items-center gap-10 md:grid grid-cols-2 ">
-        <div className="flex flex-col items-center md:items-start gap-3 ">
-          <h1 className="text-4xl text-main-black dark:text-main-white font-medium text-center md:text-left font-poppins">
+        <div className="flex flex-col items-center md:items-start gap-3 grid-col-2">
+          <h1 className="text-4xl lg:text-6xl text-main-black dark:text-main-white font-medium text-center md:text-left font-poppins">
             Desenvolvedor de <br />
             <span className="text-main-blue font-bold">software</span>
           </h1>
@@ -18,11 +18,11 @@ function LandingPage() {
           </button>
         </div>
         <img
-          src="./src/assets/main-icon.svg"
+          src="./src/images/main-icon.svg"
           alt="Imagem principal"
-          className="w-[18rem] md:w-[30rem] "
+          className="w-[18rem] md:w-[30rem] md:place-self-end row-span-2"
         />
-        <p className="text-center md:text-left text-md text-main-black dark:text-main-white leading-6 font-poppins drop-shadow-lg  md:mt-[-6rem] lg:mt-[-10rem] md:w-8/12">
+        <p className="text-center md:text-left text-md text-main-black dark:text-main-white leading-6 font-poppins drop-shadow-lg lg:w-7/12">
           Sou um desenvolvedor de software focado na resolução de problemas,
           acostumado em trabalhar com metodologias ágeis. Minha paixão está em
           transformar desafios em soluções eficientes, sempre em busca da
