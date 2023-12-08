9d5c518ac4ed75254a57357cc8ed0e7f0c3d25ed
Júlio Nepomuceno <julioreplay@gmail.com>
2023-12-07 21:06:16 -0300

    [fix]

--- src/pages/lading-page/index.tsx
+++ src/pages/lading-page/index.tsx
@@ -4,8 +4,8 @@ function LandingPage() {
   return (
     <div className="bg-main-white dark:bg-dark-main-black h-screen px-10">
       <Header />
-      <main className="mt-20 flex flex-col items-center md:flex-row gap-10 ">
-        <div className="flex flex-col items-center md:items-start gap-3">
+      <main className="mt-20 flex flex-col items-center gap-10 md:grid grid-cols-2 ">
+        <div className="flex flex-col items-center md:items-start gap-3 ">
           <h1 className="text-4xl text-main-black dark:text-main-white font-medium text-center md:text-left font-poppins">
             Desenvolvedor de <br />
             <span className="text-main-blue font-bold">software</span>
@@ -13,16 +13,16 @@ function LandingPage() {
           <span className="text-main-black dark:text-main-white text-xl font-normal tracking-widest">
             Júlio Nepomuceno
           </span>
-          <button className="h-10 border-2 mt-2 px-10 md:px-8 border-main-blue text-main-blue hover:bg-gray-100 rounded-full font-poppins">
+          <button className="h-10 border-2 mt-2 px-10 md:px-8 border-main-blue text-main-blue hover:bg-gray-100 dark:hover:bg-dark-hover-black rounded-full font-poppins">
             BAIXAR CURRÍCULO
           </button>
         </div>
         <img
           src="./src/assets/main-icon.svg"
           alt="Imagem principal"
-          className="w-[18rem] md:w-[60rem]"
+          className="w-[18rem] md:w-[30rem] "
         />
-        <p className="text-center text-md text-main-black dark:text-main-white leading-6 font-poppins drop-shadow-lg ">
+        <p className="text-center md:text-left text-md text-main-black dark:text-main-white leading-6 font-poppins drop-shadow-lg  md:mt-[-6rem] lg:mt-[-10rem] md:w-8/12">
           Sou um desenvolvedor de software focado na resolução de problemas,
           acostumado em trabalhar com metodologias ágeis. Minha paixão está em
           transformar desafios em soluções eficientes, sempre em busca da
