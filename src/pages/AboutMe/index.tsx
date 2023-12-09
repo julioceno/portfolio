326a0e8ea66fea8c38179454169010452dbfb149
Júlio Nepomuceno <julioreplay@gmail.com>
2023-12-09 19:23:47 -0300

    [feature]: implement wrap in aboutMe

--- src/pages/AboutMe/index.tsx
+++ src/pages/AboutMe/index.tsx
@@ -3,7 +3,7 @@ import { useRef } from "react";
 import BookImage from "../../../public/book.png";
 import GymImage from "../../../public/gym.png";
 import ProfileImage from "../../../public/profile.png";
-import { Header, Footer } from "../../components";
+import { Wrap } from "../../components";
 import { useFollowPointer } from "./hooks";
 
 const paragraphs = [
@@ -33,65 +33,61 @@ export function AboutMe() {
   const { x, y } = useFollowPointer(ref);
 
   return (
-    <div className="bg-main-white dark:bg-dark-main-black min-h-screen flex flex-col">
-      <section className="flex-1 px-10 xl:px-48 ">
-        <Header />
-        <main className="md:mx-10 2xl:mx-64 flex flex-col gap-4 mb-6">
-          <div className="xsm:overflow-hidden flex flex-row justify-center pt-16 items-center gap-3 sm:w-full">
-            <motion.img
-              src={GymImage}
-              animate={{ x, y }}
-              transition={{
-                type: "spring",
-                damping: 30,
-                stiffness: 30,
-                restDelta: 0.001,
-              }}
-              className="p-1 w-14 xsm:w-20 sm:w-32 md:w-36 lg:w-48 rounded-lg image-with-shadow"
-            />
-            <motion.img
-              src={ProfileImage}
-              ref={ref}
-              animate={{ x, y }}
-              transition={{
-                type: "spring",
-                damping: 30,
-                stiffness: 20,
-                restDelta: 0.001,
-              }}
-              className="z-2 w-14 xsm:w-20 sm:w-32 md:w-36 lg:w-48 mb-10 rounded-lg image-with-shadow"
-            />
-            <motion.img
-              src={BookImage}
-              animate={{ x, y }}
-              transition={{
-                type: "spring",
-                damping: 30,
-                stiffness: 30,
-                restDelta: 0.001,
-              }}
-              className="p-1 w-14 xsm:w-20 sm:w-32 md:w-36 lg:w-48 rounded-lg image-with-shadow"
-            />
-          </div>
-          <div className="flex flex-col gap-6">
-            {paragraphs.map((text, index) => {
-              const value = index % 2;
-              const oddNumber = !value;
+    <Wrap>
+      <div className="md:mx-10 2xl:mx-64 flex flex-col gap-4 mb-6">
+        <div className="xsm:overflow-hidden flex flex-row justify-center pt-16 items-center gap-3 sm:w-full">
+          <motion.img
+            src={GymImage}
+            animate={{ x, y }}
+            transition={{
+              type: "spring",
+              damping: 30,
+              stiffness: 30,
+              restDelta: 0.001,
+            }}
+            className="p-1 w-14 xsm:w-20 sm:w-32 md:w-36 lg:w-48 rounded-lg image-with-shadow"
+          />
+          <motion.img
+            src={ProfileImage}
+            ref={ref}
+            animate={{ x, y }}
+            transition={{
+              type: "spring",
+              damping: 30,
+              stiffness: 20,
+              restDelta: 0.001,
+            }}
+            className="z-2 w-14 xsm:w-20 sm:w-32 md:w-36 lg:w-48 mb-10 rounded-lg image-with-shadow"
+          />
+          <motion.img
+            src={BookImage}
+            animate={{ x, y }}
+            transition={{
+              type: "spring",
+              damping: 30,
+              stiffness: 30,
+              restDelta: 0.001,
+            }}
+            className="p-1 w-14 xsm:w-20 sm:w-32 md:w-36 lg:w-48 rounded-lg image-with-shadow"
+          />
+        </div>
+        <div className="flex flex-col gap-6">
+          {paragraphs.map((text, index) => {
+            const value = index % 2;
+            const oddNumber = !value;
 
-              return (
-                <p
-                  className={`text-sm md:text-base text-main-black dark:text-main-white font-medium font-poppins text-center ${
-                    oddNumber ? "md:text-left" : "md:text-end"
-                  }`}
-                >
-                  {text}
-                </p>
-              );
-            })}
-          </div>
-        </main>
-      </section>
-      <Footer />
-    </div>
+            return (
+              <p
+                className={`text-sm md:text-base text-main-black dark:text-main-white font-medium font-poppins text-center ${
+                  oddNumber ? "md:text-left" : "md:text-end"
+                }`}
+              >
+                {text}
+              </p>
+            );
+          })}
+        </div>
+      </div>
+    </Wrap>
   );
 }
