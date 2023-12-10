406699b0a83bc7e24e095dce9953bdf94af39995
Júlio Nepomuceno <julioreplay@gmail.com>
2023-12-10 14:08:37 -0300

    [feature]: adjusting breakpoints

--- src/pages/ladingPage/index.tsx
+++ src/pages/ladingPage/index.tsx
@@ -16,14 +16,14 @@ const LandingPage = observer(() => {
       <div className="flex-1 flex justify-center items-center mt-5 md:mt-0">
         <main className=" flex flex-col items-center gap-10 md:grid grid-cols-2 ">
           <div className="flex flex-col items-center md:items-start gap-3 grid-col-2">
-            <h1 className="text-4xl lg:text-5xl xl:text6xl text-main-black dark:text-main-white font-medium text-center md:text-left font-poppins">
+            <h1 className="text-2xl xsm:text-4xl lg:text-5xl xl:text6xl text-main-black dark:text-main-white font-medium text-center md:text-left font-poppins">
               Desenvolvedor de <br />
               <span className="text-main-blue font-bold">software</span>
             </h1>
-            <span className="text-main-black dark:text-main-white text-xl font-normal tracking-widest">
+            <span className="text-main-black dark:text-main-white text-1xs md:text-xl font-normal tracking-widest">
               Júlio Nepomuceno
             </span>
-            <button className="h-10 border-2 mt-2 px-10 md:px-8 border-main-blue text-main-blue hover:bg-gray-100 dark:hover:bg-dark-hover-black rounded-full font-poppins">
+            <button className="h-10 border-2 mt-2 px-10 md:px-8 border-main-blue text-main-blue hover:bg-gray-100 dark:hover:bg-dark-hover-black rounded-full font-poppins text-sm md:text-base">
               BAIXAR CURRÍCULO
             </button>
           </div>
@@ -33,7 +33,7 @@ const LandingPage = observer(() => {
             className="w-[18rem] md:w-[30rem] md:place-self-end row-span-2"
           />
           <p
-            className={`text-center md:text-left text-md text-main-black dark:text-main-white leading-6 font-poppins lg:w-7/12 ${classTextShadow}`}
+            className={`text-center md:text-left text-sm md:text-base text-main-black dark:text-main-white leading-6 font-poppins lg:w-7/12 ${classTextShadow}`}
           >
             Sou um desenvolvedor de software focado na resolução de problemas,
             acostumado em trabalhar com metodologias ágeis. Minha paixão está em
