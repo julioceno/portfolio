8b1c9542f96607a9b300d31e5ad6c238f6b2dc4e
Julio <julio.nepomuceno@eurekalabs.com.br>
2023-12-27 23:03:27 -0300

    [feature]: import default company image

--- src/pages/ProfessionalJourney/professional-journey.ts
+++ src/pages/ProfessionalJourney/professional-journey.ts
@@ -1,6 +1,7 @@
 import { TechEnum } from '../../components';
 import { IProfessionalJourneyProps, WorkinDayEnum } from './types';
 
+import DefaultCompanyIcon from '../../../public/companies/default-company.svg';
 import EurekaLogoIcon from '../../../public/companies/eureka.svg';
 
 export const professionalJourney: IProfessionalJourneyProps[] = [
@@ -11,8 +12,7 @@ export const professionalJourney: IProfessionalJourneyProps[] = [
       'Tornei-me parte fundamental da equipe ao prestar serviços para uma das maiores transportadoras do país. Meu foco está em implementar soluções inovadoras que agregam valor ao cliente e aprimoram a eficiência dos fluxos internos. Tenho o prazer de colaborar com meus colegas, oferecendo suporte e compartilhando ideias para alcançarmos resultados excepcionais. Estou sempre em busca de novos desafios e oportunidades para crescer profissionalmente.Tornei-me parte fundamental da equipe ao prestar serviços para uma das maiores transportadoras do país. Meu foco está em implementar soluções inovadoras que agregam valor ao cliente e aprimoram a eficiência dos fluxos internos. Tenho o prazer de colaborar com meus colegas, oferecendo suporte e compartilhando ideias para alcançarmos resultados excepcionais. Estou sempre em busca de novos desafios e oportunidades para crescer profissionalmente.',
     companyName: 'Programing Network',
     skills: ['Metodologias Ágeis', 'Análises Técnicas'],
-    startIn: new Date(2021, 10, 1),
-    endIn: new Date(2023, 0, 1),
+    startIn: new Date(2023, 0, 1),
     techs: [
       TechEnum.REACT,
       TechEnum.MATERIAL_UI,
@@ -48,7 +48,7 @@ export const professionalJourney: IProfessionalJourneyProps[] = [
     workinDay: WorkinDayEnum.PART_TIME,
   },
   {
-    image: EurekaLogoIcon,
+    image: DefaultCompanyIcon,
     jobRole: 'Desenvolvedor Mobile',
     preview:
       'Atuei na Programming Network trazendo soluções digitais através de aplicativos desde sua prototipação até a criação das linhas de código. Atuando também em analises técnicas, criação de propostas e atuando na conversação com o cliente.',
