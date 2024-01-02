a6dfb23cce7c910619b6a72581c5635f5a7f439c
Julio <julio.nepomuceno@eurekalabs.com.br>
2024-01-02 10:05:45 -0300

    [feature]: adjusting layout large

--- src/components/Wrap/index.tsx
+++ src/components/Wrap/index.tsx
@@ -9,14 +9,16 @@ interface Props {
 
 export function Wrap({ children, ocultFooter }: Props) {
   return (
-    <div
-      className={`bg-main-white dark:bg-dark-main-black h-screen flex flex-col overflow-y-auto ${SCROLLBAR_DEFAULT}`}
-    >
-      <section className='flex flex-col flex-1 px-10 xl:px-48'>
-        <Header />
-        <main className='flex-1 flex'>{children}</main>
-      </section>
-      {!ocultFooter && <Footer />}
+    <div className={`bg-main-white dark:bg-dark-main-black h-screen`}>
+      <div
+        className={`h-full flex flex-col overflow-y-auto max-w-[2000px] mx-auto ${SCROLLBAR_DEFAULT}`}
+      >
+        <section className='flex flex-col flex-1 px-10 xl:px-48'>
+          <Header />
+          <main className='flex-1 flex'>{children}</main>
+        </section>
+        {!ocultFooter && <Footer />}
+      </div>
     </div>
   );
 }
