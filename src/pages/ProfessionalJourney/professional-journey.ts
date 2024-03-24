9f671c4ff69a4f3c5329099e6722c2b507b2e572
Júlio Nepomuceno <65461945+julioceno@users.noreply.github.com>
2024-03-24 11:21:58 -0300

    [fix]: adjusting company name

--- src/pages/ProfessionalJourney/professional-journey.ts
+++ src/pages/ProfessionalJourney/professional-journey.ts
@@ -10,7 +10,7 @@ export const professionalJourney: IProfessionalJourneyProps[] = [
     jobRole: 'Desenvolvedor Fullstack',
     preview:
       'Tenho a honra de contribuir de maneira significativa com minhas equipes, agora atuando em tempo integral. Tive o prazer de continuar atuando para uma das maiores empresas de agenciamento de cargas do país. Mas levando meus conhecimentos para outros projetos, minha dedicação me proporcionou uma visão mais abrangente dentro da empresa. Atuando em projetos em seguradora renomada na América latina, atuando em sistemas internos para melhorar o fluxo de trabalho e tendo uma participação em sistemas bancários.',
-    companyName: 'Programing Network',
+    companyName: 'Eureka Labs',
     skills: [
       'Mentoria',
       'Adaptabilidade entre Projetos',
@@ -47,7 +47,7 @@ export const professionalJourney: IProfessionalJourneyProps[] = [
     jobRole: 'Desenvolvedor Fullstack',
     preview:
       'Tornei-me parte fundamental da equipe ao prestar serviços para uma das maiores transportadoras do país. Meu foco está em implementar soluções inovadoras que agregam valor ao cliente e aprimoram a eficiência dos fluxos internos. Tenho o prazer de colaborar com meus colegas, oferecendo suporte e compartilhando ideias para alcançarmos resultados excepcionais. Estou sempre em busca de novos desafios e oportunidades para crescer profissionalmente.',
-    companyName: 'Programing Network',
+    companyName: 'Eureka Labs',
     skills: [
       'Proatividade na Aprendizagem',
       'Resolução de Problemas',
